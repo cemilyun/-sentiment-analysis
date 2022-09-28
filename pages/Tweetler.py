f5813ed57aa82fc106744a26124a0668f5909836
Cem İlyün <79357300+cemilyun@users.noreply.github.com>
2022-09-28 16:08:33 +0300

    Update Tweetler.py

--- pages/Tweetler.py
+++ pages/Tweetler.py
@@ -7,10 +7,12 @@ import plotly.graph_objects as go
 from tensorflow import keras
 from tensorflow.python.keras.preprocessing.sequence import pad_sequences
 from tensorflow.python.keras.preprocessing.text import Tokenizer
+import tensorflow as tf
 import numpy as np
 import pickle
+import itertools
 from tensorflow.keras.models import load_model
-
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 def loadModel(aList):
     model = load_model('model.h5')
     with open('tokenizer.pickle', 'rb') as handle:
@@ -22,16 +24,17 @@ def loadModel(aList):
     predd = np.where(predd > 0.6, 1, 0)
     i = 0
     return_list=[]
+    olumlu = []
+    olumsuz = []
     while i < len(predd):
         if predd[i] == 1:
-            print("olumlu")
+            olumlu.append(aList[i])
             return_list.append(predd[i])
         else:
-            print("olumsuz")
+            olumsuz.append(aList[i])
             return_list.append(predd[i])
         i += 1
-    return return_list
-
+    return return_list, olumlu, olumsuz
 
 
 st.title("Sentiment Analysis")
@@ -49,13 +52,14 @@ if st.button('Çalıştır') and tweet != "":
         tweetJson = [json.loads(line) for line in open(tweet + ".json", 'r', encoding="utf-8")]
         expander = st.expander("Tweetler")
         for i in range(len(tweetJson)):
-            expander.write(str(i) + tweetJson[i]['tweet'])
+            expander.write(str(i)+". "+ tweetJson[i]['tweet'])
             st.write()
     except FileNotFoundError as err:
         st.error("Böyle bir dosya bulunamadı.")
 
 elif tweet == "":
     st.error("Lütfen bir tweet giriniz.")
+
 
 analysis = st.selectbox("Analizi yapılacak tweeti seçiniz",dosyalar)
 analysis = analysis.replace(" ", "_")
@@ -69,7 +73,7 @@ if st.button('Analiz Yap') and analysis != "":
     for i in range(len(analysisJson)):
         tweetler.append(analysisJson[i]['tweet'])
     st.success(f"Analiz başarıyla tamamlandı. Analiz edilen tweet sayısı: {len(tweetler)}")
-    yeni = loadModel(tweetler)
+    yeni, olumlu, olumsuz = loadModel(tweetler)
     for i in yeni:
         if i == 1:
             pozCount += 1
@@ -81,6 +85,14 @@ if st.button('Analiz Yap') and analysis != "":
     fig = go.Figure(go.Pie(labels=countList, values=values, hoverinfo="label+percent",textinfo="value"))
     st.header("Analiz:")
     st.plotly_chart(fig)
+    olumluTweets = st.expander("Olumlu Tweetler")
+    for i in range(len(olumlu)):
+        olumluTweets.write(str(i) + ". " + olumlu[i])
+        st.write()
+    olumsuzTweets = st.expander("Olumsuz Tweetler")
+    for i in range(len(olumsuz)):
+        olumsuzTweets.write(str(i) + ". " + olumsuz[i])
+        st.write()
 elif analysis == "":
     st.error("Lütfen analiz yapılacak tweet giriniz.")
 
